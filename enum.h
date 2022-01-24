11da0771e6f3fa9963033b0f5ab85e84ae5b38f2
EricDiGi <etd7@students.uwf.edu>
2022-01-24 15:13:44 -0600

    try again

--- enum.h
+++ enum.h
@@ -5,23 +5,23 @@
 #include <stdlib.h>
 
 
-typedef enum {Segment, Line, typeConstant, typeId, typeOperator, Comment} nodeEnum;
+//typedef enum {Segment, Line, typeConstant, typeId, typeOperator, Comment} nodeEnum;
 
 // Constants
 typedef struct{
-    nodeEnum type;
+    //nodeEnum type;
     int value;
 } constantNodeType;
 
 // Identifiers
 typedef struct{
-    nodeEnum type;
+    //nodeEnum type;
     char name[50];
 } idNodeType;
 
 // Operators
 typedef struct{
-    nodeEnum type;
+    //nodeEnum type;
     int operator;
     int numOps;
     union nodeTypeTag *op[1];
@@ -49,7 +49,7 @@ typedef struct{
 // Parts-of-speech
 typedef struct{
     Segment seg;
-    nodeEnum type;
+    //nodeEnum type;
 } Comment;
 
 void initSymbolTable

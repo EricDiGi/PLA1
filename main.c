55c78573e78aaeae18643f31ccdaa2070c91727f
EricDiGi <etd7@students.uwf.edu>
2022-01-24 12:33:27 -0600

    good starting point

--- main.c
+++ main.c
@@ -1,5 +1,30 @@
 #include <iostream>
+#include <stdio.h>
+#include <stdlib.h>
 
-int main(){
+
+void extend(char &input){
     
+}
+
+
+int main(int argc, char **argv){
+    FILE* file_pointer;
+    fp = open(argv[1], "r");
+
+    char* ch = new char(1);
+    int curr_ = 0;
+    while((ch[curr_] = fgetc(file_pointer)) != EOF)
+        // send to storage?
+        printf("%c", ch[curr_]);
+        char* next = new char(curr_+2);
+        for(int i = 0; i <= curr_; i++)
+            next[i] = ch[i];
+        ch = next;
+        //delete next;
+    fclose(file_pointer);
+
+    for(int i = 0; i <= curr_; i++)
+        printf("%c",ch[i]);
+
 }

1d67719abe6f7c1c13022b08e77843f4c7b2b6f0
EricDiGi <etd7@students.uwf.edu>
2022-01-24 12:48:28 -0600

    arrays instead of pointers

--- main.c
+++ main.c
@@ -12,17 +12,13 @@ int main(int argc, char **argv){
     FILE* file_pointer;
     file_pointer = fopen(argv[1], "r");
 
-    char* ch = new char(1);
-    char* next = new char(0);
+    char ch[1];
     int curr_ = 0;
     while((ch[curr_] = fgetc(file_pointer)) != EOF){
-        // send to storage?
-        //printf("%c", ch[curr_]);
-        next = new char(curr_+2);
+        char next[curr_+1];
         for(int i = 0; i <= curr_; i++)
             next[i] = ch[i];
         ch = next;
-        //delete next;
     }
     fclose(file_pointer);
 

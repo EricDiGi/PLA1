b564f0000452ec3bb0fb42d8ae86ff6d58843f5d
EricDiGi <etd7@students.uwf.edu>
2022-01-24 13:11:56 -0600

    test again

--- main.c
+++ main.c
@@ -12,18 +12,13 @@ int main(int argc, char **argv){
     FILE* file_pointer;
     file_pointer = fopen(argv[1], "r");
 
-    char ch[1];
+    fseek(file_pointer, 0, SEEK_END);
+    int file_length = ftell(file_pointer);
+    fseek(file_pointer, 0, SEEK_SET);
+
+    char ch[file_length];
     int curr_ = 0;
-    while((ch[curr_] = fgetc(file_pointer)) != EOF){
-        char next[curr_+1];
-        for(int i = 0; i <= curr_; i++)
-            next[i] = ch[i];
-        strcpy(next, ch);
-        curr_++;
-        //printf("%c", next[curr_]);
-    }
-    for(int i = 0; i <= curr_; i++)
-        printf("%c",ch[i]);
+    while((ch[curr_] = fgetc(file_pointer)) != EOF){ curr_++;}
     fclose(file_pointer);
 
 }

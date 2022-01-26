75c941ca7f3a7af9800a4d37fc27cd362814344e
EricDiGi <etd7@students.uwf.edu>
2022-01-26 12:29:41 -0600

    works on comments

--- Lexer.h
+++ Lexer.h
@@ -59,9 +59,11 @@ bool lexan(char* string, int length, int depth){
     if(next_tok > -1){
         char token[next_tok];
         char remainder[length-next_tok];
-        printf("next token: %d", next_tok);
-        strncpy(token, &string[0], next_tok-1);
+        printf("next token: %d\n", next_tok);
+
+        strncpy(token, &string[0], next_tok);
         printf("token: "); printString(token);
+
         strncpy(remainder, &string[next_tok], length);
         printf("remainder: "); printString(remainder);
         /*bool lexan_resolute = false;
